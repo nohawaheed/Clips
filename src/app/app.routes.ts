c3ed31c46f6aa5a8ae466e0e3b07b7156982cc61
nohawaheed <nohawaheed.1991@gmail.com>
2026-05-05 03:45:19 +0300

    feat: add routes and guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
+import { Home } from './views/home/home';
+import { About } from './views/about/about';
+import { Manage } from './views/manage/manage';
+import { Upload } from './views/upload/upload';
+import { Clip } from './views/clip/clip';
+import { NotFound } from './views/not-found/not-found';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
-export const routes: Routes = [];
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'manage',
+    component: Manage,
+    data: {
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
+    },
+    canActivate: [AuthGuard],
+
+  },
+  {
+    path: 'upload',
+    component: Upload,
+    data: {
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
+    },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'clip/:id',
+    component: Clip,
+  },
+  {
+    path: '**',
+    component: NotFound
+  }
+];
