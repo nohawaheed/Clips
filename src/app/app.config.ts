b837faa4a14b60115b997b136cbd5f892352785f
nohawaheed <nohawaheed.1991@gmail.com>
2026-05-05 03:42:11 +0300

    feat: add firebase configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyArs6ER5R_T1JMgcmYYAs5atqzIleyOwXM",
+      authDomain: "clips-6786e.firebaseapp.com",
+      projectId: "clips-6786e",
+      storageBucket: "clips-6786e.firebasestorage.app",
+      messagingSenderId: "1028335369289",
+      appId: "1:1028335369289:web:72f9b7657a7d1121b01398"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./core/nav/nav";
+import { AuthModal } from "./user/auth-modal/auth-modal";
+import { AuthService } from './services/auth';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, AuthModal, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('clips');
+  auth = inject(AuthService);
 }
