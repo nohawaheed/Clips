d27a4befeec28493818dacc2c7724a85654225ee
nohawaheed <nohawaheed.1991@gmail.com>
2026-05-05 04:48:24 +0300

    refactor: remove firebase configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,12 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp({
-      apiKey: "AIzaSyArs6ER5R_T1JMgcmYYAs5atqzIleyOwXM",
-      authDomain: "clips-6786e.firebaseapp.com",
-      projectId: "clips-6786e",
-      storageBucket: "clips-6786e.firebasestorage.app",
-      messagingSenderId: "1028335369289",
-      appId: "1:1028335369289:web:72f9b7657a7d1121b01398"
+
     })),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
